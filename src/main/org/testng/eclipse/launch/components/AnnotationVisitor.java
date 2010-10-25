043548091aa5e08677c657eb9fab88eda0dce763
Cedric Beust <cedric@beust.com>
2010-10-26 02:42:08 +0800

    Adding a few generics.

--- src/main/org/testng/eclipse/launch/components/AnnotationVisitor.java
+++ src/main/org/testng/eclipse/launch/components/AnnotationVisitor.java
@@ -70,9 +70,9 @@ public class AnnotationVisitor extends BaseVisitor {
         m_annotationType = JDK15_ANNOTATION;
       }
       
-      List pairs = node.values();
-      for (Iterator it = pairs.iterator(); it.hasNext(); ) {
-        MemberValuePair mvp = (MemberValuePair) it.next();
+      @SuppressWarnings("unchecked")
+      List<MemberValuePair> pairs = node.values();
+      for (MemberValuePair mvp : pairs) {
         Name attribute = mvp.getName();
         String name = attribute.getFullyQualifiedName();
         if ("groups".equals(name)) {
@@ -80,9 +80,9 @@ public class AnnotationVisitor extends BaseVisitor {
           // Array?
           if (value instanceof ArrayInitializer) {
             ArrayInitializer ai = (ArrayInitializer) value;
-            List expressions = ai.expressions();
-            for (Iterator it2 = expressions.iterator(); it2.hasNext(); ) {
-              Expression e = (Expression) it2.next();
+            @SuppressWarnings("unchecked")
+            List<Expression> expressions = ai.expressions();
+            for (Expression e : expressions) {
               addGroup(e.toString());
             }
           }
