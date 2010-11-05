862fcde9daf057d52e67212154e7ff3dda32db15
Asiel Brumfield <asielb@gmail.com>
2010-11-05 14:15:27 -0600

    fixed xmlTemplate to use file dialog

--- src/main/org/testng/eclipse/properties/TestNGPropertyPage.java
+++ src/main/org/testng/eclipse/properties/TestNGPropertyPage.java
@@ -27,6 +27,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.ISelectionStatusValidator;
@@ -114,8 +115,7 @@ public class TestNGPropertyPage extends PropertyPage {
     {
       SelectionAdapter buttonListener = new SelectionAdapter() {
         public void widgetSelected(SelectionEvent evt) {
-          DirectoryDialog dlg= new DirectoryDialog(m_xmlTemplateFile.getShell());
-          dlg.setMessage("Select Template XML file");
+          FileDialog dlg= new FileDialog(m_xmlTemplateFile.getShell());          
           String selectedDir= dlg.open();
           m_xmlTemplateFile.setText(selectedDir != null ? selectedDir : "");
         }
